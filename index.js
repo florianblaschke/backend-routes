fc9b2ff474256ef83822c4146054bfb452624968
Florian Blaschke <florianblaschke@web.de>
2023-06-28 11:05:48 +0200

    finished

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Fish is on http://127.0.0.1:${port}/`);
+});
